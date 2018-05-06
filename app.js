3df431eb479559ffa5d127fe97e407b91f1e9856
Mike <munkyu0813@gmail.com>
2018-05-06 09:37:08 -0400

    hook up passport.js to app with googleOAuth

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const { mongoose } = require('./db/mongoose');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +14,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('It works!');
 });
+
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
